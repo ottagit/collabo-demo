28a9eba33d3e6542f9847093613f92777ca40891
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-04-02 17:21:33 +0300

    print hashed trangle of size n

---
+++ hash-triangle.js
@@ -0,0 +1,6 @@
+function triangle(n){
+  for (var i = '#'; i.length <= n; i+="#") {//loops through and print i while adding a hash each time the loopstarted
+    console.log(i);
+  }
+}
+triangle(8);
